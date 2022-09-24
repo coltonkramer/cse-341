5600cfcb886ef30e9d8b4f9f46ce919518dc4546
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-09-24 15:56:26 -0400

    made some changes

--- assignment2/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-// GET /feed/posts
-router.use('/contacts', require('./contacts'));
-// localhost:8080/professional/
-module.exports = router;
